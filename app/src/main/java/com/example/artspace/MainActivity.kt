bfc3f3b02c8212bdf94c402e84561a1db13a0047
nguch <kelvin.1.ngure@gmail.com>
2023-04-01 14:57:19 +0300

    skeleton. composables and state variables set up

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,41 +3,48 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ArtSpaceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            ArtSpaceApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ArtSpaceApp(modifier: Modifier = Modifier){
+    val Images = listOf(R.drawable.baobab, R.drawable.cheetah, R.drawable.elephant, R.drawable.hills, R.drawable.grassland, R.drawable.kilimanjaro_girraffe, R.drawable.zebra)
+    var currentImage by remember {
+        mutableStateOf(0)
+    }
+
+    Column {
+        ArtWork()
+        Description()
+        NavigationButton()
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ArtSpaceTheme {
-        Greeting("Android")
-    }
+fun ArtWork(modifier: Modifier = Modifier){
+
+}
+
+@Composable
+fun Description(modifier: Modifier = Modifier){
+
+}
+
+@Composable
+fun NavigationButton(modifier: Modifier = Modifier){
+
 }
