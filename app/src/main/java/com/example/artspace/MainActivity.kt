dc75982f57090e242386c383f3e1e1b2383e7e40
nguch <kelvin.1.ngure@gmail.com>
2023-04-01 15:13:04 +0300

    artworks surface set up. able to access the image list and current image state variable

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,21 +26,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceApp(modifier: Modifier = Modifier){
-    val Images = listOf(R.drawable.baobab, R.drawable.cheetah, R.drawable.elephant, R.drawable.hills, R.drawable.grassland, R.drawable.kilimanjaro_girraffe, R.drawable.zebra)
+    val artWorks: List<Int> = listOf(R.drawable.baobab, R.drawable.cheetah, R.drawable.elephant, R.drawable.hills, R.drawable.grassland, R.drawable.kilimanjaro_girraffe, R.drawable.zebra)
     var currentImage by remember {
         mutableStateOf(0)
     }
 
     Column {
-        ArtWork()
+        ArtWork(artWorks, currentImage)
         Description()
         NavigationButton()
     }
 }
 
 @Composable
-fun ArtWork(modifier: Modifier = Modifier){
-
+fun ArtWork(artWorks: List<Int>, currentImage: Int, modifier: Modifier = Modifier){
+    Surface(
+        elevation = 10.dp,
+    ) {
+        Image(painter = painterResource(id = artWorks[currentImage]), contentDescription = "art")
+    }
 }
 
 @Composable
